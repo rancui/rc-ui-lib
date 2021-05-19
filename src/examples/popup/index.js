a32c920073596a8194aafa1067858c028f81a778
rancui <271242038@qq.com>
2021-05-19 11:28:43 +0800

    chore: 更改目录名大小写

---
+++ src/examples/popup/index.js
@@ -0,0 +1,55 @@
+import React, { PureComponent } from 'react';
+import Button from '@/components/button';
+import Popup from '@/components/popup';
+export default class PopupComponent extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            show: false
+        };
+    }
+
+    show = () => {
+        this.setState({
+            show: true
+        });
+    };
+
+    close = () => {
+        this.setState({
+            show: false
+        });
+    };
+
+    onClickCloseIcon = () => {
+        console.log('onClickCloseIcon');
+        this.setState({
+            show: false
+        });
+    };
+
+    render() {
+        return (
+            <div className="popup">
+                <Button onClick={this.show} type="primary">
+                    Show Popup
+                </Button>
+                <Popup
+                    closeIcon={require('@/assets/images/user-active.png')}
+                    closeIconPosition="top-right"
+                    closeable
+                    onClickCloseIcon={this.onClickCloseIcon}
+                    onClose={this.close}
+                    // position="center"
+                    position="left"
+                    round
+                    safeAreaInsetBottom
+                    show={this.state.show}
+                    // teleport="#root"
+                >
+                    <h1>hello,popup!</h1>
+                </Popup>
+            </div>
+        );
+    }
+}
