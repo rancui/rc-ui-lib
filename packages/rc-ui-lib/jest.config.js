b09f819db5a145d2bab21904114dc010c5cfdedd
rancui <271242038@qq.com>
2021-11-18 11:07:53 +0800

    feat: 增加collectCoverageFrom配置项

--- packages/rc-ui-lib/jest.config.js
+++ packages/rc-ui-lib/jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   setupFiles: ['./tests/setup.js'],
   setupFilesAfterEnv: ['./tests/setupAfterEnv.ts'],
   testPathIgnorePatterns: ['/es/', '/lib/', '<rootDir>/node_modules/'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/hooks/**',
+    '!src/utils/**',
+    '!**/demo/**',
+    '!src/**/PropsType.ts',
+    '!src/**/index.tsx',
+  ],
 };
