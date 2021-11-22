c5046251c5f4a6b6aa8ae4f0e36e396a271b6c85
rancui <271242038@qq.com>
2021-11-22 11:21:35 +0800

    test: 增加组件ConfigProvider的单元测试

---
+++ packages/rc-ui-lib/src/config-provider/__test__/index.spec.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { sleep } from '../../../tests/utils';
+import Icon from '../../icon';
+import ConfigProvider from '..';
+
+describe('ConfigProvider', () => {
+  let wrapper;
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  it('when using themeVars prop', () => {
+    const themeVars = {
+      color: 'red',
+    };
+    wrapper = mount(
+      <ConfigProvider themeVars={themeVars}>
+        <Icon name="success" />
+      </ConfigProvider>,
+    );
+    expect(toJson(wrapper.html())).toMatchSnapshot();
+  });
+
+  it('when using iconPrefix prop', () => {
+    wrapper = mount(
+      <ConfigProvider iconPrefix="my-icon">
+        <Icon name="success" />
+      </ConfigProvider>,
+    );
+    expect(wrapper.find('i.my-icon-success').exists()).toBeTruthy();
+  });
+});
