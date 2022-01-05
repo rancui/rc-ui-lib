b39c26d999950fe27671442fcb6e2e1fff7c7406
pengjin <pengjin@vcredit.com>
2022-01-05 11:32:36 +0800

    test: 新增PasswordInput组件单元测试

--- packages/rc-ui-lib/src/password-input/__test__/index.spec.tsx
+++ packages/rc-ui-lib/src/password-input/__test__/index.spec.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { fireEvent, render, cleanup, act } from '@testing-library/react';
-import { PasswordInput, PasswordInputProps } from '..';
+import React, { useRef } from 'react';
+import { fireEvent, render, cleanup, waitFor } from '@testing-library/react';
+import { PasswordInput, PasswordInputInstance, PasswordInputProps } from '..';
 import { NumberKeyboard } from '../../number-keyboard';
 import TestsEvent from '../../../tests/events';
 
@@ -47,21 +47,39 @@ describe('PasswordInput', () => {
     expect(onFocus).toHaveBeenCalled();
   });
 
-  // it('should emit onBlur event after clicking outside', async () => {
-  //   const onBlur = jest.fn();
-  //   const props = {
-  //     ...$props,
-  //     onBlur,
-  //   };
+  it('should emit onFocus event when wrapper is focused', async () => {
+    const onFocus = jest.fn();
+    const props = {
+      ...$props,
+      onFocus,
+    };
 
-  //   const { container } = createPasswordInput(props);
-  //   const wrapper = container.querySelector('.rc-password-input__security');
-  //   await TestsEvent.triggerTouch(wrapper, 'touchstart', [[0, 0]]);
+    const { container } = createPasswordInput(props);
+    const wrapper = container.querySelector('.rc-password-input');
+    await fireEvent.focus(wrapper);
+    expect(onFocus).toHaveBeenCalled();
+  });
 
-  //   await clickKey(document.body);
+  it('should emit onBlur event after clicking outside', async () => {
+    const onBlur = jest.fn();
+    const props = {
+      ...$props,
+      focused: true,
+      keyboard: <NumberKeyboard theme="custom" />,
+      onBlur,
+    };
 
-  //   expect(onBlur).toHaveBeenCalled();
-  // });
+    const { getAllByRole, container } = createPasswordInput(props);
+    const keyBlur = getAllByRole('button')[9];
+    const keyClose = getAllByRole('button')[12];
+    const wrapper = container.querySelector('.rc-password-input');
+
+    await clickKey(keyBlur);
+    await clickKey(keyClose);
+    await fireEvent.blur(wrapper);
+
+    expect(onBlur).toHaveBeenCalled();
+  });
 
   it('should emit onChange event after input number', async () => {
     const onChange = jest.fn();
@@ -74,6 +92,22 @@ describe('PasswordInput', () => {
 
     const { getAllByRole } = createPasswordInput(props);
     const key = getAllByRole('button')[0];
+    await clickKey(key);
+    expect(onChange).toHaveBeenCalledWith('1');
+  });
+
+  it('should emit onChange event after press delete button', async () => {
+    const onChange = jest.fn();
+    const props = {
+      ...$props,
+      value: '12',
+      focused: true,
+      onChange,
+      keyboard: <NumberKeyboard />,
+    };
+
+    const { getAllByRole } = createPasswordInput(props);
+    const key = getAllByRole('button')[11];
     await clickKey(key);
     expect(onChange).toHaveBeenCalledWith('1');
   });
@@ -105,20 +139,95 @@ describe('PasswordInput', () => {
     };
 
     const { container } = createPasswordInput(props);
-
     expect(container).toMatchSnapshot();
   });
 
-  // it('should emit onDelete event after clicking delete key', async () => {
-  //   const onDelete = jest.fn();
-  //   const props = {
-  //     ...$props,
-  //     onDelete,
-  //   };
+  it('should render correctly when using mask prop', async () => {
+    const props = {
+      ...$props,
+      value: '1',
+      mask: false,
+    };
 
-  //   const { getAllByRole } = createPasswordInput(props);
-  //   const key = getAllByRole('button')[11];
-  //   await clickKey(key);
-  //   expect(onDelete).toHaveBeenCalled();
-  // });
+    const { getByText } = createPasswordInput(props);
+    const item = getByText('1');
+    expect(item).toMatchSnapshot();
+  });
+
+  it('should render correctly when using gutter prop', async () => {
+    const props = {
+      ...$props,
+      gutter: 10,
+    };
+
+    const { container } = createPasswordInput(props);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render correctly when using length prop', async () => {
+    const props = {
+      ...$props,
+      length: 4,
+    };
+
+    const { container, rerender } = createPasswordInput(props);
+    expect(container).toMatchSnapshot();
+    const props2 = {
+      ...$props,
+      length: 6,
+      value: undefined,
+    };
+    await waitFor(() => rerender(<PasswordInput {...props2} />));
+    expect(container).toMatchSnapshot();
+  });
+
+  it("test instance's function", async () => {
+    const onBlur = jest.fn();
+    const onFocus = jest.fn();
+    const onChange = jest.fn();
+    const Demo = () => {
+      const passwordInputRef = useRef<PasswordInputInstance>(null);
+
+      const handleFocus = () => {
+        passwordInputRef.current?.focus();
+      };
+
+      const handleBlur = () => {
+        passwordInputRef.current?.blur();
+      };
+      const props = {
+        ...$props,
+        onFocus,
+        onBlur,
+        onChange,
+      };
+
+      return (
+        <div>
+          <PasswordInput ref={passwordInputRef} {...props} />,
+          <button type="button" data-testid="button1" onClick={handleFocus}>
+            handleFocus
+          </button>
+          <button type="button" data-testid="button2" onClick={handleBlur}>
+            handleBlur
+          </button>
+        </div>
+      );
+    };
+
+    const { container, getByTestId } = render(<Demo />);
+
+    const buttonBox1 = getByTestId('button1');
+    const buttonBox2 = getByTestId('button2');
+
+    await fireEvent.click(buttonBox1);
+    expect(onFocus).toHaveBeenCalled();
+
+    const input = container.querySelector('.rc-password-input__native-input');
+    fireEvent.change(input, { target: { value: '123' } });
+    expect(onChange).toHaveBeenCalledWith('123');
+
+    fireEvent.click(buttonBox2);
+    expect(onBlur).toHaveBeenCalled();
+  });
 });
