0ed372ca08e0bcbccd9b98d26be38f4e50771fef
windole <1322670109@qq.com>
2022-01-29 15:43:45 +0800

    feat: 新增picker组件
    feat: 新增picker组件

---
+++ packages/rc-ui-lib/src/picker/PickerColumn.tsx
@@ -0,0 +1,252 @@
+import React, {
+  forwardRef,
+  useCallback,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+} from 'react';
+import { useSpring, animated } from '@react-spring/web';
+import { useDrag } from '@use-gesture/react';
+import classNames from 'classnames';
+import { useSetState, useUpdateEffect } from '../hooks';
+import { isObject, range } from '../utils';
+import { deepClone } from '../utils/deep-clone';
+import { rubberbandIfOutOfBounds } from '../utils/rubberband';
+import { Column, PickerColumnProps, PickerOption } from './PropsType';
+import ConfigProviderContext from '../config-provider/ConfigProviderContext';
+
+function isOptionDisabled(option) {
+  return isObject(option) && option.disabled;
+}
+
+const DEFAULT_DURATION = 200;
+const MOMENTUM_LIMIT_DISTANCE = 15;
+
+const PickerColumn = forwardRef<{}, PickerColumnProps>((props, ref) => {
+  const { prefixCls, createNamespace } = useContext(ConfigProviderContext);
+  const [bem] = createNamespace('picker-column', prefixCls);
+
+  const { itemHeight, visibleItemCount, defaultIndex, initialOptions } = props;
+
+  const root = useRef<HTMLDivElement>(null);
+  const wrapper = useRef<HTMLUListElement>(null);
+
+  const indexRef = useRef<number>(defaultIndex);
+  const optionsRef = useRef(deepClone(initialOptions));
+
+  const [state, updateState] = useSetState({
+    offset: 0,
+    duration: 0,
+    options: deepClone(initialOptions),
+  });
+
+  const count = useMemo(() => optionsRef.current.length, [optionsRef.current.length]);
+
+  const baseOffset = useMemo(() => {
+    // 默认转入第一个选项的位置
+    return (itemHeight * (+visibleItemCount - 1)) / 2;
+  }, [itemHeight, visibleItemCount]);
+
+  const draggingRef = useRef(false);
+
+  const [{ y }, api] = useSpring(() => ({
+    from: { y: defaultIndex * -itemHeight + baseOffset },
+    config: {
+      tension: 400,
+      mass: 0.8,
+    },
+  }));
+
+  const adjustIndex = (index: number): number | null => {
+    index = range(index, 0, count);
+    for (let i = index; i < count; i += 1) {
+      if (!isOptionDisabled(optionsRef.current[i])) return i;
+    }
+    // for (let i = index - 1; i >= 0; i -= 1) {
+    //   if (!isOptionDisabled(optionsRef.current[i])) return i;
+    // }
+
+    return null;
+  };
+
+  const setIndex = (index: number, emitChange?: boolean) => {
+    index = adjustIndex(index) || 0;
+
+    const offset = index * -itemHeight;
+    api.start({ y: offset + baseOffset });
+
+    if (index !== indexRef.current) {
+      indexRef.current = index;
+      // updateState({ index });
+      if (emitChange && props.onChange) {
+        setTimeout(() => {
+          props.onChange(index);
+        }, 0);
+      }
+    }
+
+    updateState({ offset });
+  };
+  const setOptions = (options: Column) => {
+    if (JSON.stringify(options) !== JSON.stringify(optionsRef.current)) {
+      optionsRef.current = options;
+      setIndex(props.defaultIndex);
+    }
+  };
+
+  const onClickItem = (index: number) => {
+    if (draggingRef.current || props.readonly) {
+      return;
+    }
+    updateState({ duration: DEFAULT_DURATION });
+    setIndex(index, true);
+  };
+
+  const getOptionText = (option: []) => {
+    if (isObject(option) && props.textKey in option) {
+      return option[props.textKey];
+    }
+    return option;
+  };
+
+  const getIndexByOffset = (offset: number) =>
+    range(Math.round(-offset / props.itemHeight), 0, count - 1);
+
+  const bind = useDrag(
+    (dragState) => {
+      if (props.readonly) {
+        return;
+      }
+      draggingRef.current = true;
+      if (dragState.tap) {
+        updateState({ duration: 0, offset: dragState.offset[1] });
+      } else if (dragState.last) {
+        const allowMomentum =
+          Math.abs(dragState.velocity[1] * dragState.direction[1] * 50) > MOMENTUM_LIMIT_DISTANCE;
+        const position =
+          dragState.offset[1] + dragState.velocity[1] * dragState.direction[1] * 50 - baseOffset;
+        const targetIndex = getIndexByOffset(position);
+        updateState({
+          duration: allowMomentum ? +props.swipeDuration : DEFAULT_DURATION,
+          offset: position + baseOffset,
+        });
+        setIndex(targetIndex, true);
+      } else {
+        const position = dragState.offset[1];
+        const max = baseOffset;
+        const min = -(count * itemHeight) + baseOffset;
+        // const offset = range(position, -(count * itemHeight), itemHeight);
+        const offset = rubberbandIfOutOfBounds(position, min, max, itemHeight * 50, 0.2);
+        api.start({
+          y: offset,
+        });
+      }
+    },
+    {
+      axis: 'y',
+      from: () => [0, y.get()],
+      filterTaps: true,
+      pointer: { touch: true },
+    },
+  );
+
+  const renderOptions = useMemo(() => {
+    const optionStyle = {
+      height: `${props.itemHeight}px`,
+    };
+
+    return optionsRef.current.map((option, index: number) => {
+      const text = getOptionText(option);
+      const disabled = isOptionDisabled(option);
+
+      const data = {
+        role: 'button',
+        key: index,
+        style: optionStyle,
+        tabIndex: disabled ? -1 : 0,
+        className: classNames(
+          bem('item', {
+            disabled,
+            selected: index === indexRef.current,
+          }),
+        ),
+        onClick: () => {
+          draggingRef.current = false;
+          onClickItem(index);
+        },
+      };
+
+      const childData = {
+        className: 'rc-ellipsis',
+        children: text,
+      };
+
+      return (
+        <li {...data}>
+          {props.optionRender ? props.optionRender(option) : <div {...childData} />}
+        </li>
+      );
+    });
+  }, [optionsRef.current, indexRef.current, onClickItem, props.optionRender]);
+
+  const setValue = (value: string) => {
+    const options = optionsRef.current;
+    for (let i = 0; i < options.length; i += 1) {
+      if (getOptionText(options[i]) === value) {
+        return setIndex(i);
+      }
+    }
+    return null;
+  };
+
+  const getValue = useCallback<() => PickerOption>(
+    () => optionsRef.current[indexRef.current],
+    [indexRef.current, optionsRef.current],
+  );
+
+  useEffect(() => {
+    setIndex(defaultIndex);
+  }, [defaultIndex]);
+
+  useUpdateEffect(() => {
+    setOptions(deepClone(initialOptions));
+  }, [initialOptions]);
+
+  useImperativeHandle(ref, () => ({
+    state,
+    getIndex: () => indexRef.current,
+    getOptions: () => optionsRef.current,
+    setIndex,
+    getValue,
+    setValue,
+    setOptions,
+  }));
+
+  return (
+    <div ref={root} className={classNames(bem(), props.className)} {...bind()}>
+      <animated.ul
+        ref={wrapper}
+        className={classNames(bem('wrapper'))}
+        style={{
+          y,
+          transitionDuration: `${state.duration}ms`,
+          transitionProperty: state.duration ? 'all' : 'none',
+        }}
+        aria-hidden
+      >
+        {renderOptions}
+      </animated.ul>
+    </div>
+  );
+});
+
+PickerColumn.defaultProps = {
+  initialOptions: [],
+  defaultIndex: 0,
+};
+
+PickerColumn.displayName = 'PickerColumn';
+
+export default PickerColumn;
---
+++ packages/rc-ui-lib/src/picker/__test__/index.spec.tsx
@@ -0,0 +1,324 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+import React from 'react';
+import { render, cleanup, act, fireEvent } from '@testing-library/react';
+import TestsEvent from '../../../tests/events';
+import { sleep } from '../../../tests/utils';
+import Picker, { PickerInstance, PickerProps } from '..';
+
+const simpleColumn = ['1990', '1991', '1992', '1993', '1994', '1995'];
+const columns = [
+  {
+    values: ['vip', 'normal'],
+    className: 'column1',
+  },
+  {
+    values: simpleColumn,
+    className: 'column2',
+  },
+];
+
+describe('Picker', () => {
+  function createPicker(props?: Partial<PickerProps>) {
+    const pickerRef = React.createRef<PickerInstance>();
+
+    const { queryByTestId, container, rerender, debug } = render(
+      <Picker ref={pickerRef} {...props} />,
+    );
+
+    return {
+      container,
+      rerender,
+      pickerRef,
+      debug,
+      queryByTestId,
+    };
+  }
+
+  afterEach(() => {
+    cleanup();
+    jest.restoreAllMocks();
+  });
+
+  it('simple columns confirm & cancel event', async () => {
+    const onConfirm = jest.fn();
+    const onCancel = jest.fn();
+
+    const { container } = createPicker({
+      showToolbar: true,
+      columns: simpleColumn,
+      onConfirm,
+      onCancel,
+    });
+
+    const confirmBtn = container.querySelector('.rc-picker__confirm');
+    const cancelBtn = container.querySelector('.rc-picker__cancel');
+
+    fireEvent.click(confirmBtn);
+    fireEvent.click(cancelBtn);
+
+    expect(onConfirm).toHaveBeenCalledWith('1990', 0);
+    expect(onCancel).toHaveBeenCalledWith('1990', 0);
+  });
+
+  it('multiple columns confirm & cancel event', async () => {
+    const onConfirm = jest.fn();
+    const onCancel = jest.fn();
+
+    const { container } = createPicker({
+      showToolbar: true,
+      columns,
+      onConfirm,
+      onCancel,
+    });
+
+    const confirmBtn = container.querySelector('.rc-picker__confirm');
+    const cancelBtn = container.querySelector('.rc-picker__cancel');
+
+    fireEvent.click(confirmBtn);
+    fireEvent.click(cancelBtn);
+
+    expect(onConfirm).toHaveBeenCalledWith(['vip', '1990'], [0, 0]);
+    expect(onCancel).toHaveBeenCalledWith(['vip', '1990'], [0, 0]);
+  });
+
+  test('drag columns', async () => {
+    const onChange = jest.fn();
+
+    const { container } = createPicker({
+      columns,
+      onChange,
+    });
+
+    const column = container.querySelector('.rc-picker-column__wrapper');
+    await TestsEvent.triggerDrag(column, [0, 0]);
+    await TestsEvent.triggerDrag(column, [0, -100]);
+
+    await sleep(500);
+    expect(onChange).toHaveBeenCalledWith(['normal', '1990'], 0);
+  });
+
+  test('drag simple columns', async () => {
+    const onChange = jest.fn();
+
+    const { container } = createPicker({
+      columns: simpleColumn,
+      onChange,
+    });
+
+    const column = container.querySelector('.rc-picker-column__wrapper');
+    await TestsEvent.triggerDrag(column, [0, -100]);
+
+    await sleep(500);
+    expect(onChange).toHaveBeenCalledWith('1995', 5);
+  });
+
+  test('column watch default index', async () => {
+    const disabled = { disabled: true, text: 1 };
+    const props: PickerProps = {
+      columns: [disabled, ...simpleColumn],
+      itemHeight: 50,
+      visibleItemCount: 5,
+      swipeDuration: 1000,
+    };
+    const { container, rerender } = createPicker(props);
+    expect(container).toMatchSnapshot();
+
+    const props2: PickerProps = {
+      ...props,
+      defaultIndex: 2,
+    };
+    rerender(<Picker {...props2} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should render title correctly', async () => {
+    const { container } = createPicker({
+      columns,
+      title: 'Custom title',
+    });
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should render toolbar correctly', async () => {
+    const { container } = createPicker({
+      columns,
+      toolbar: 'Custom toolbar',
+    });
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should render option correctly', async () => {
+    const { container } = createPicker({
+      showToolbar: true,
+      columns: ['foo', 'bar'],
+      optionRender: (item) => item,
+    });
+    expect(container).toMatchSnapshot();
+  });
+
+  // test('click column item', async () => {
+  //   const onChange = jest.fn();
+  //   const columns = [
+  //     { text: '杭州' },
+  //     { text: '宁波' },
+  //     { text: '温州', disabled: true },
+  //     { text: '嘉兴', disabled: true },
+  //   ];
+  //   const { container, debug } = createPicker({
+  //     columns,
+  //     onChange,
+  //   });
+
+  //   const columnWrapper = container.querySelector('.rc-picker-column');
+  //   await fireEvent.click(columnWrapper);
+
+  //   const column = container.querySelectorAll('.rc-picker-column__item')[2];
+  //   debug();
+  //   await TestsEvent.triggerDrag(column, [0, 0]);
+  //   // await fireEvent.click(column);
+  //   expect(onChange).toHaveBeenCalledWith(columns[1], 1);
+  // });
+
+  test('toolbar-position prop', () => {
+    const { container } = createPicker({
+      showToolbar: true,
+      toolbarPosition: 'bottom',
+      columns,
+    });
+    expect(container).toMatchSnapshot();
+  });
+
+  test('columns-top、columns-bottom prop', () => {
+    const { container } = createPicker({
+      columnsTop: 'Custom Columns Top',
+      columnsBottom: 'Custom Columns Bottom',
+      columns,
+    });
+    expect(container).toMatchSnapshot();
+  });
+
+  test('watch columns change', () => {
+    const onConfirm = jest.fn();
+    const props: PickerProps = {
+      showToolbar: true,
+      columns: ['1', '2'],
+      defaultIndex: 1,
+      onConfirm,
+    };
+    const { container, rerender } = createPicker(props);
+    const props2 = {
+      ...props,
+      columns: ['2', '3'],
+    };
+    rerender(<Picker {...props2} />);
+
+    fireEvent.click(container.querySelector('.rc-picker__confirm'));
+    expect(onConfirm).toHaveBeenCalledWith('3', 1);
+  });
+
+  test('should not reset index when columns unchanged', async () => {
+    const onConfirm = jest.fn();
+    const props: PickerProps = {
+      showToolbar: true,
+      columns: ['1', '2'],
+      onConfirm,
+    };
+    const { container, rerender, pickerRef } = createPicker(props);
+    act(() => {
+      pickerRef.current.setIndexes([1]);
+    });
+    const props2 = {
+      ...props,
+      columns: ['1', '2'],
+    };
+    rerender(<Picker {...props2} />);
+
+    fireEvent.click(container.querySelector('.rc-picker__confirm'));
+    expect(onConfirm).toHaveBeenCalledWith('2', 1);
+  });
+
+  test('set rem item-height', () => {
+    const originGetComputedStyle = window.getComputedStyle;
+
+    window.getComputedStyle = () => ({ fontSize: '16px' } as CSSStyleDeclaration);
+    const { container } = createPicker({
+      columns: simpleColumn.slice(0, 2),
+      itemHeight: '10rem',
+    });
+    expect(container).toMatchSnapshot();
+
+    window.getComputedStyle = originGetComputedStyle;
+  });
+
+  test('readonly prop', async () => {
+    const onChange = jest.fn();
+    const { container } = createPicker({
+      columns,
+      readonly: true,
+      onChange,
+    });
+
+    const column = container.querySelectorAll('.rc-picker-column__item')[3];
+    await fireEvent.click(column);
+
+    const columnWrapper = container.querySelector('.rc-picker-column__wrapper');
+    await TestsEvent.triggerDrag(columnWrapper, [0, -100]);
+
+    expect(onChange).not.toHaveBeenCalled();
+  });
+
+  test('set picker values', () => {
+    const onConfirm = jest.fn();
+    const props: PickerProps = {
+      showToolbar: true,
+      columns,
+      onConfirm,
+    };
+    const { pickerRef } = createPicker(props);
+
+    const vm = pickerRef.current;
+
+    expect(vm.getColumnValues(-1)).toEqual(undefined);
+    expect(vm.getColumnValues(1)).toHaveLength(6);
+    expect(vm.getColumnValue(1)).toEqual('1990');
+
+    act(() => {
+      vm.setColumnValue(0, 'normal');
+    });
+    expect(vm.getColumnValue(0)).toEqual('normal');
+
+    act(() => {
+      vm.setColumnIndex(0, 0);
+    });
+    expect(vm.getColumnValue(0)).toEqual('vip');
+
+    act(() => {
+      vm.setColumnValue(1, '1991');
+    });
+    expect(vm.getColumnValue(1)).toEqual('1991');
+
+    act(() => {
+      vm.setColumnValues(0, ['vip', 'normal', 'other']);
+    });
+    expect(vm.getColumnValues(0)).toHaveLength(3);
+    expect(vm.getValues()).toHaveLength(2);
+
+    act(() => {
+      vm.setColumnValues(-1, []);
+    });
+    expect(vm.getValues()).toHaveLength(2);
+
+    act(() => {
+      vm.setValues(['vip', '1992']);
+    });
+    expect(vm.getColumnIndex(1)).toEqual(2);
+    expect(vm.getColumnIndex(2)).toEqual(0);
+    expect(vm.getIndexes()).toEqual([0, 2]);
+
+    act(() => {
+      vm.setIndexes([1, 4]);
+    });
+    expect(vm.getColumnValue(1)).toEqual('1994');
+    expect(vm.getColumnValue(2)).toEqual(undefined);
+  });
+});
