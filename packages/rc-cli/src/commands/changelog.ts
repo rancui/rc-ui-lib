e2890460467435942b895a4068fa8e5c876479f4
windole <windole321@163.com>
2023-08-09 18:40:32 +0800

    chore(changelog): 新增日志自动生成功能

--- packages/rc-cli/src/commands/changelog.ts
+++ packages/rc-cli/src/commands/changelog.ts
@@ -12,6 +12,7 @@ const DIST_FILE = join(ROOT, './changelog.generated.md');
 const HEADER_TEMPLATE = join(__dirname, '../../template/changelog-header.hbs');
 const MAIN_TEMPLATE = join(__dirname, '../../template/changelog-main.hbs');
 const COMMIT_TEMPLATE = join(__dirname, '../../template/changelog-commit.hbs');
+const CHANGELOG_MD = join(ROOT, './docs/markdown/changelog.zh-CN.md');
 
 const mainTemplate = readFileSync(MAIN_TEMPLATE, 'utf-8');
 const headerPartial = readFileSync(HEADER_TEMPLATE, 'utf-8');
@@ -58,6 +59,9 @@ export async function changelog(): Promise<void> {
         preset: 'angular',
         releaseCount: 2,
       },
+      null,
+      null,
+      null,
       {
         mainTemplate,
         headerPartial,
@@ -66,9 +70,32 @@ export async function changelog(): Promise<void> {
       },
     )
       .pipe(createWriteStream(DIST_FILE))
-      .on('close', () => {
+      .on('close', async () => {
         spinner.succeed(`Changelog generated at ${slimPath(DIST_FILE)}`);
+        await updateChangelog(spinner);
         resolve();
       });
+  });
+}
+
+async function updateChangelog(spinner: ora.Ora) {
+  fse.readFile(CHANGELOG_MD, (err, data) => {
+    if (err) throw err;
+
+    const content = data.toString();
+    const regex = /## 更新内容/g;
+    const match = regex.exec(content);
+    if (!match) {
+      console.log('未找到更新内容');
+      return;
+    }
+    const generatedText = readFileSync(DIST_FILE);
+    const insertIndex = match.index + match[0].length + 4;
+    const updatedContent =
+      content.slice(0, insertIndex) + generatedText + '\r\n' + content.slice(insertIndex);
+    fse.writeFile(CHANGELOG_MD, updatedContent, 'utf8', (err) => {
+      if (err) throw err;
+      spinner.succeed(`更新日志插入成功`);
+    });
   });
 }
